8b9010b6a2d986877a456cebc160d171a64e30c4
5586658 <5586658@U702834.univali.br>
2018-11-21 21:32:18 -0200

    ...

--- InspecaoCelesc/src/inspecaocelesc/InspecaoCelesc.java
+++ InspecaoCelesc/src/inspecaocelesc/InspecaoCelesc.java
@@ -5,15 +5,26 @@
  */
 package inspecaocelesc;
 
+import Core.*;
 import SGBD.Conexao;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
  * @author marco
  */
 public class InspecaoCelesc {
-
+    static private Date getData(String str){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dateTime = LocalDate.parse(str, formatter);
+        Date a = java.sql.Date.valueOf(dateTime);
+        
+        return a;
+    }
     /**
      * @param args the command line arguments
      */
@@ -21,7 +32,16 @@ public class InspecaoCelesc {
     public static void main(String[] args) {
         // TODO code application logic here
         Connection con = new Conexao().getConnection();
-  
+        Poste teste = new Poste();
+        Inspecao teste2 = new Inspecao();
+        String str = "13/12/2015";
+        
+        
+        try {
+            teste2.inserirInspecao(con, true, true, true, getData(str), 1);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
